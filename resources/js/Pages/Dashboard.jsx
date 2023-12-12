6d1226850af59b1cf553db2cfdbc5e07873e5843
ivandez <ivan.mendez.dev@gmail.com>
2023-12-12 13:31:49 -0400

    change class to className

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -17,7 +17,7 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="relative overflow-x-auto">
+                        <div className="relative overflow-x-auto">
                             <Table />
                         </div>
                     </div>
--- resources/js/Pages/components/Table.jsx
+++ resources/js/Pages/components/Table.jsx
@@ -1,38 +1,38 @@
 export default function Table() {
     return (
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col" className="px-6 py-3">
                         Cédula
                     </th>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col" className="px-6 py-3">
                         Nombres
                     </th>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col" className="px-6 py-3">
                         Apellidos
                     </th>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col" className="px-6 py-3">
                         teléfono
                     </th>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col" className="px-6 py-3">
                         Dirreción
                     </th>
                 </tr>
             </thead>
             <tbody>
                 {[1, 2, 3].map((item) => (
-                    <tr class="bg-white dark:bg-gray-800">
+                    <tr className="bg-white dark:bg-gray-800">
                         <th
                             scope="row"
-                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                         >
                             26589996
                         </th>
-                        <td class="px-6 py-4">Pablo Pablo</td>
-                        <td class="px-6 py-4">Gomez Gomez</td>
-                        <td class="px-6 py-4">+584245895693</td>
-                        <td class="px-6 py-4">Valencia Carabobo</td>
+                        <td className="px-6 py-4">Pablo Pablo</td>
+                        <td className="px-6 py-4">Gomez Gomez</td>
+                        <td className="px-6 py-4">+584245895693</td>
+                        <td className="px-6 py-4">Valencia Carabobo</td>
                     </tr>
                 ))}
             </tbody>
