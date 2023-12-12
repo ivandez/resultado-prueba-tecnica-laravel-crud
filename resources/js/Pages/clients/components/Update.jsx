a070e840c231f587b15921e204fe249f322d281f
ivandez <ivan.mendez.dev@gmail.com>
2023-12-12 17:29:13 -0400

    added validations

--- resources/js/Pages/clients/components/Update.jsx
+++ resources/js/Pages/clients/components/Update.jsx
@@ -53,7 +53,7 @@ export default function Update({ client }) {
                     Cédula
                 </label>
                 <input
-                    type="text"
+                    type="number"
                     id="ci"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                     {...register("ci")}
