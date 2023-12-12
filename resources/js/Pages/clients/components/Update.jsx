c5d84d72b8dfe39abab8b0c99f60889213884ff8
ivandez <ivan.mendez.dev@gmail.com>
2023-12-12 15:17:29 -0400

    refactor routes

--- resources/js/Pages/clients/components/Update.jsx
+++ resources/js/Pages/clients/components/Update.jsx
@@ -97,7 +97,7 @@ export default function Update({ client }) {
                 Submit
             </button>
             <Link
-                href="/dashboard"
+                href="/"
                 className="ml-2 text-white bg-red-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
             >
                 Cancelar
