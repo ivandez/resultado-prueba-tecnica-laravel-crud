ceb4c9fec1d0142240b3d18cf18295e54e4b2bd4
ivandez <ivan.mendez.dev@gmail.com>
2023-12-12 15:33:33 -0400

    added confirmation modal

--- resources/js/Pages/components/Table.jsx
+++ resources/js/Pages/components/Table.jsx
@@ -5,6 +5,15 @@ export default function Table({ clients }) {
     const deleteClient = (id) => {
         router.delete(`/client/${id}`);
     };
+
+    const askDeleteClient = (id) => {
+        const result = confirm("¿Desea eliminar a este cliente?");
+
+        if (result) {
+            deleteClient(id);
+        }
+    };
+
     return (
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -50,7 +59,7 @@ export default function Table({ clients }) {
                                 Editar
                             </Link>
                             <button
-                                onClick={() => deleteClient(client.id)}
+                                onClick={() => askDeleteClient(client.id)}
                                 type="button"
                                 className="text-white bg-red-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
                             >
