c71c82bc698ef6b514fca75796f42ad1f27b8451
ivandez <ivan.mendez.dev@gmail.com>
2023-12-12 11:50:49 -0400

    create client controller

--- database/migrations/2023_12_12_152222_create_clients_table.php
+++ database/migrations/2023_12_12_152222_create_clients_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('flights', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('cellphone_number');
+            $table->string('address');
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('flights');
+        Schema::dropIfExists('clients');
     }
 };
