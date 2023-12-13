24c17b0d08b795a680bdff3661436476a1185790
ivandez <ivan.mendez.dev@gmail.com>
2023-12-12 20:14:04 -0400

    wip search table

--- resources/js/Pages/components/Table.jsx
+++ resources/js/Pages/components/Table.jsx
@@ -1,6 +1,16 @@
 import { Link } from "@inertiajs/react";
 import { router } from "@inertiajs/react";
-import { CompactTable } from "@table-library/react-table-library/compact";
+import {
+    Table,
+    Header,
+    HeaderRow,
+    Body,
+    Row,
+    HeaderCell,
+    Cell,
+    useCustom,
+} from "@table-library/react-table-library/table";
+import { useState } from "react";
 
 const COLUMNS = [
     { label: "Cédula", renderCell: (item) => item.ci },
@@ -42,87 +52,75 @@ const COLUMNS = [
     },
 ];
 
-export default function Table({ clients }) {
+export default function Table2({ clients }) {
     const nodes = [...clients];
 
-    const data = { nodes };
+    // const data = { nodes };
 
     const deleteClient = (id) => {
         router.delete(`/client/${id}`);
     };
 
-    const askDeleteClient = (id) => {
-        const result = confirm("¿Desea eliminar a este cliente?");
+    // const askDeleteClient = (id) => {
+    //     const result = confirm("¿Desea eliminar a este cliente?");
 
-        if (result) {
-            deleteClient(id);
-        }
+    //     if (result) {
+    //         deleteClient(id);
+    //     }
+    // };
+
+    const [search, setSearch] = useState("");
+
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+    };
+
+    const data = {
+        nodes: nodes.filter((item) =>
+            item.first_name.toLowerCase().includes(search.toLowerCase())
+        ),
     };
 
     return (
-        <div className="w-full">
-            <CompactTable columns={COLUMNS} data={data} />
-        </div>
-        // <table className=" text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        //     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-        //         <tr>
-        //             <th scope="col" className="px-6 py-3">
-        //                 Cédula
-        //             </th>
-        //             <th scope="col" className="px-6 py-3">
-        //                 Nombres
-        //             </th>
-        //             <th scope="col" className="px-6 py-3">
-        //                 Apellidos
-        //             </th>
-        //             <th scope="col" className="px-6 py-3">
-        //                 teléfono
-        //             </th>
-        //             <th scope="col" className="px-6 py-3">
-        //                 Dirreción
-        //             </th>
-        //             <th scope="col" className="px-6 py-3">
-        //                 Acciones
-        //             </th>
-        //         </tr>
-        //     </thead>
-        //     <tbody>
-        //         {clients.map((client) => (
-        //             <tr className="bg-white dark:bg-gray-800" key={client.id}>
-        //                 <th
-        //                     scope="row"
-        //                     className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-        //                 >
-        //                     {client.ci}
-        //                 </th>
-        //                 <td className="px-6 py-4">{client.first_name}</td>
-        //                 <td className="px-6 py-4">{client.last_name}</td>
-        //                 <td className="px-6 py-4">{client.cellphone_number}</td>
-        //                 <td className="px-6 py-4">{client.address}</td>
-        //                 <td className="px-6 py-4">
-        //                     <Link
-        //                         href={`/client/${client.id}/edit`}
-        //                         className="text-white bg-green-700  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-        //                     >
-        //                         Editar
-        //                     </Link>
-        //                     <button
-        //                         onClick={() => askDeleteClient(client.id)}
-        //                         type="button"
-        //                         className="text-white bg-red-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-        //                     >
-        //                         Eliminar
-        //                     </button>
-        //                     <Link
-        //                         href={`client/${client.id}`}
-        //                         className="text-white bg-blue-700  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-        //                     >
-        //                         Inspeccionar
-        //                     </Link>
-        //                 </td>
-        //             </tr>
-        //         ))}
-        //     </tbody>
-        // </table>
+        <>
+            <label htmlFor="search">
+                Search by Task:&nbsp;
+                <input
+                    id="search"
+                    type="text"
+                    value={search}
+                    onChange={handleSearch}
+                />
+            </label>
+            <br />
+
+            <Table data={data}>
+                {(tableList) => (
+                    <>
+                        <Header>
+                            <HeaderRow>
+                                <HeaderCell>Task</HeaderCell>
+                                <HeaderCell>Deadline</HeaderCell>
+                                <HeaderCell>Type</HeaderCell>
+                                <HeaderCell>Complete</HeaderCell>
+                                <HeaderCell>Tasks</HeaderCell>
+                            </HeaderRow>
+                        </Header>
+
+                        <Body>
+                            {tableList.map((item) => (
+                                <Row key={item.id} item={item}>
+                                    <Cell>{item.first_name}</Cell>
+                                    <Cell>test</Cell>
+                                    <Cell>test</Cell>
+                                    <Cell>test</Cell>
+                                    <Cell>test</Cell>
+                                </Row>
+                            ))}
+                        </Body>
+                    </>
+                )}
+            </Table>
+        </>
     );
 }
